4590794e2126774b610a4de2ad871e0f4d7f8579
1082204 <57616789+1082204@users.noreply.github.com>
2020-05-28 19:27:00 +0200

    Hej

--- distributionplus/assets/js/script.js
+++ distributionplus/assets/js/script.js
@@ -1,10 +1,26 @@
 // Initialize and add the map
 function initMap() {
     // The location of DistributionPLUS
-    var DistributionPLUS = {lat: 57.112277, lng: 9.852163};
+    var DistributionPLUS = { lat: 57.112277, lng: 9.852163 };
     // The map, centered at DistributionPLUS
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 17, center: DistributionPLUS});
+        document.getElementById('map'), { zoom: 17, center: DistributionPLUS });
     // The marker, positioned at DistributionPLUS
-    var marker = new google.maps.Marker({position: DistributionPLUS, map: map});
-  }
+    var marker = new google.maps.Marker({ position: DistributionPLUS, map: map });
+};
+
+// Video preview i loesninger //
+
+
+
+
+document.querySelectorAll(".play-video").forEach(videobox => {
+    videobox.querySelectorAll(".preview").addEventListener("mouseover", function () {
+        this.play()
+    })
+
+    videobox.querySelectorAll(".preview").addEventListener("mouseover", function () {
+        this.pause()
+    })
+
+});
