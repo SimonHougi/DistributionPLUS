2d61629a131262051b653f1a57da237a0920dbe9
1082204 <57616789+1082204@users.noreply.github.com>
2020-06-09 21:41:12 +0200

    DAHG

--- distributionplus/assets/js/script.js
+++ distributionplus/assets/js/script.js
@@ -133,7 +133,7 @@ function validate() // Funktion der tjekker, om vigtige oplysninger er indtastet
 }
 
 
-// Script til burgermenu //
+// Script til burgermenu - inspiration fra workshop med DAHG //
 
 function toggleClass(selector, className){
     document.querySelector(selector).classList.toggle(className); // Dette script er en togglefunction, således nogle classer bliver aktive/inaktive //
